3912a9646ba82ee38306c3996b6c97ef11aed5c9
alexanderfreitag <ge78zil@mytum.de>
2024-06-27 17:54:43 +0200

    add language detection

---
+++ language_detection.py
@@ -0,0 +1,19 @@
+from langdetect import detect, DetectorFactory, LangDetectException
+
+# Ensure consistent results
+DetectorFactory.seed = 0
+
+
+def detect_language(text):
+    try:
+        # Detect the language of the input text
+        detected_lang = detect(text)
+        lang_map = {
+            'de': 'German',
+            'en': 'English',
+            'bg': 'Bulgarian'
+        }
+        # Return either "Bulgarian", "German", "English" or "Unknown" if it isn't one of the three languages.
+        return lang_map.get(detected_lang, 'Unknown')
+    except LangDetectException:
+        return 'Unknown'
